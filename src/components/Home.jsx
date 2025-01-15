ad06c2f536cc9ca4c5ceb125b067e66ccae26818
Swastik Mohapatra <swastik.mohapatra@incture.com>
2025-01-15 16:20:49 +0530

    contact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,68 +2,85 @@
 import Typewriter from "typewriter-effect";
 // import AIPIC from "../assets/images/comp5.png";
 import Spline from "@splinetool/react-spline";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
-function Home({mainControls}) {
+function Home({ mainControls }) {
   return (
     <div name="home">
-      <motion.div 
-       variants={{
-        hidden:{opacity:0,y:75},
-        visible:{opacity:1,y:0}
-       }}
-       initial="hidden"
-       animate={mainControls}
-       transition={{duration:0.5, delay:0.25}}
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
       >
-      <div className="py-10">
-        <div className="md:flex md:justify-between md:items-center flex-none grid grid-cols-1 md:grid-cols-0 gap-2">
-          <div className="ml-0 md:ml-6 md:mt-5 leading-relaxed">
-            <h1 className="text-4xl font-bold leading-normal">Hi,</h1>
-            <h1 className="text-4xl font-bold leading-normal">
-              I am Swastik Mohapatra
-            </h1>
-            <h1 className="text-4xl font-bold leading-normal">
-              <Typewriter
-                options={{
-                  strings: ["Software Engineer", "Front End Developer"],
-                  autoStart: true,
-                  pauseFor: 4000,
-                  loop: true,
-                }}
-              />
-            </h1>
-            <br />
-            <p className="flex flex-col justify-center">
-              Crafting seamless user experiences through code mastery – Welcome
-              to the digital frontier of web design and software architecture.
-            </p>
+        <div className="py-10">
+          <div className="md:flex md:justify-between md:items-center flex-none grid grid-cols-1 md:grid-cols-0 gap-2">
+            <div className="ml-0 md:ml-6 md:mt-5 leading-relaxed">
+              <h1 className="text-4xl font-bold leading-normal">Hi,</h1>
+              <h1 className="text-4xl font-bold leading-normal">
+                I am Swastik Mohapatra
+              </h1>
+              <h1 className="text-4xl font-bold leading-normal">
+                <Typewriter
+                  options={{
+                    strings: ["Software Engineer", "Front End Developer"],
+                    autoStart: true,
+                    pauseFor: 4000,
+                    loop: true,
+                  }}
+                />
+              </h1>
+              <br />
+              <p className="flex flex-col justify-center">
+                Crafting seamless user experiences through code mastery –
+                Welcome to the digital frontier of web design and software
+                architecture.
+              </p>
 
-            <div className="flex flex-row gap-6 sm:ml-[-4px] mt-7">
-            <a href="src/assets/files/Swastik-Mohapatra-Resume.pdf" target='_blank'>
-              <button href="src/assets/files/Swastik-Mohapatra-Resume.pdf" target='_blank'  className=" bg-indigo-500 text-gray-100 p-3 text-sm  rounded-lg tracking-wide font-semibold  focus:outline-none focus:shadow-outline hover:bg-white hover:text-indigo-600 shadow-lg cursor-pointer transition ease-in duration-300"> 
-                Download Resume
-              </button>
-              </a>
-              <button className="  text-gray-100 p-3 text-sm border  rounded-lg tracking-wide font-semibold  focus:outline-none focus:shadow-outline hover:bg-white hover:text-black shadow-lg cursor-pointer transition ease-in duration-300">
-                Contact me
-              </button>
+              <div className="flex flex-row gap-6 sm:ml-[-4px] mt-7">
+                <a
+                  href="src/assets/files/Swastik-Mohapatra-Resume.pdf"
+                  target="_blank"
+                >
+                  <button
+                    href="src/assets/files/Swastik-Mohapatra-Resume.pdf"
+                    target="_blank"
+                    className=" bg-indigo-500 text-gray-100 p-3 text-sm  rounded-lg tracking-wide font-semibold  focus:outline-none focus:shadow-outline hover:bg-white hover:text-indigo-600 shadow-lg cursor-pointer transition ease-in duration-300"
+                  >
+                    Download Resume
+                  </button>
+                </a>
+                <button
+                  onClick={() => {
+                    const contactSection =
+                      document.getElementsByName("contact")[0];
+                    if (contactSection) {
+                      contactSection.scrollIntoView({ behavior: "smooth" });
+                    }
+                  }}
+                  className="  text-gray-100 p-3 text-sm border  rounded-lg tracking-wide font-semibold  focus:outline-none focus:shadow-outline hover:bg-white hover:text-black shadow-lg cursor-pointer transition ease-in duration-300"
+                >
+                  Contact me
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="ml-0 md:ml-16 justify-center items-center text-center">
-            <div className="w-full hidden md:block">
-              {/* <img
+            <div className="ml-0 md:ml-16 justify-center items-center text-center">
+              <div className="w-full hidden md:block">
+                {/* <img
                 src={AIPIC}
                 alt="My Profile"
                 className="object-cover"
                 width="650"
                 height="100"
               /> */}
-              <Spline scene="https://prod.spline.design/ZXTHTk1nEWEUSlVv/scene.splinecode" />
+                <Spline scene="https://prod.spline.design/ZXTHTk1nEWEUSlVv/scene.splinecode" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </motion.div>
     </div>
   );
